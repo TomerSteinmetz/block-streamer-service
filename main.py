23895ecc54ac0fafa1fa40e7bfbca0b398931645
Tomer Steinmetz <93766953+TomerSteinmetz@users.noreply.github.com>
2025-05-26 11:07:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from config import AppConfig
 from provider_client import ProviderClient
 from provider_manager import ProviderManager
 
-CHAINSTACK_URL = "https://nd-422-757-666.p2pify.com/0a9d79d93fb2f4a4b1e04695da2b77a7/"
-ALCHEMY_URL = f"https://eth-mainnet.g.alchemy.com/v2/BndtPhqXyOfm--Pv6NM_VlMS_vi8Pt2h"
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 def build_streamer(cfg_path):
